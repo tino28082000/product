56b659d75c67a2aed7d91ba6d0ea5f32643504a5
Weishow <tino28082000@gmail.com>
2020-10-26 15:21:36 +0800

    update

--- product.py
+++ product.py
@@ -1,6 +1,5 @@
-product = []
 i = 0
-
+product = []
 while True:
 	name = input('請輸入商品名稱：' )
 	if name == 'q':
@@ -8,11 +7,20 @@ while True:
 	price = input('請輸入商品價格：' )
 	price = int(price)
 	i = i + price
+	price = str(price)
 	n_p = []
 	n_p.append(name)
 	n_p.append(price)
 	product.append(n_p)
 	print('成功加入商品！！！') 
+
+for p in product:
+	print(p[0], '的價格是', p[1])
+
 print('你總共買了', product)
 print('總共是', i, '元')
+
+with open('product.txt', 'w') as d:
+	for p in product:
+		d.write(p[0] + ',' + p[1] + '\n')
 
